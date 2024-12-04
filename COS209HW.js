1cdbef0b643fbb82ea15455985b64156f0633d9f
jonnypez29 <Jonnypez29@gmail.com>
2024-12-04 10:43:43 +0100

    Add files via upload

---
+++ COS209HW.js
@@ -0,0 +1,21 @@
+let person = {
+    'Full Name': 'Akpan Jonathan Ubong',
+     Age: 42,
+    'Application ID': 'LT240060',
+    'Matric No': '24/2598' ,
+     School: 'Comupting & Engineering Sciences',
+     Course: 'Computer Science',
+    'Phone No': 9069016595 ,
+     Email: 'Jonnypez29@gmail.com',
+     Nationality: 'Nigerian',
+     Address:  {
+        Street: '14 Jaiyeoba Street,Ijegun',
+        LGA: 'Alimosho',
+        State: 'Lagos State',
+        Country: 'Nigeria'
+       }     
+                      
+    };
+  console.log(person)
+
+ 
